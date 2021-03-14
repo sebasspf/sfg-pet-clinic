08a57ad608fdb3f84d95461aaf874e19c0e569ca
Sebastián Pérez Ferretti <seperezf@gmail.com>
2021-03-14 17:16:27 -0300

    Agrego pagina index

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,43 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+	@Override
+	public Set<Owner> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return super.save(object.getId(), object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+		
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
