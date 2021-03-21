28b6fe238131961becb1c042fa23a6f92480486a
Sebastián Pérez Ferretti <seperezf@gmail.com>
2021-03-21 16:33:01 -0300

    Inicializo data. Closes #12

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
+
+	
+	@Override
+	public Set<Vet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		return super.save(object.getId(), object);
+	}
+	
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		super.delete(object);
+	}
+
+}
