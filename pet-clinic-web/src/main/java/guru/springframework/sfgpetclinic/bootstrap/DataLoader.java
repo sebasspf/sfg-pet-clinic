fb53eef9b2e0512bc529508854d9178c77a8f26a
Sebastián Pérez Ferretti <seperezf@gmail.com>
2021-03-21 16:51:29 -0300

    Configuro los servicios como beans. Closes #20

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -16,11 +16,16 @@ public class DataLoader implements CommandLineRunner{
 	private final OwnerService ownerService;
 	private final VetService vetService;
 	
-	public DataLoader() {
-		ownerService = new OwnerServiceMap();
-		vetService = new VetServiceMap();
-	}
 	
+	
+	public DataLoader(OwnerService ownerService, VetService vetService) {
+		super();
+		this.ownerService = ownerService;
+		this.vetService = vetService;
+	}
+
+
+
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
