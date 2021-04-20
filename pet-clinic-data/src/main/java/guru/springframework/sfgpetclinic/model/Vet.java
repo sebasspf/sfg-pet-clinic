214dee94b01cf505ba177e5926db12a2f8189327
Sebastián Perez Ferretti <sperez@bykom.com>
2021-04-20 11:07:41 -0300

    Mapeo con JPA los vets. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -3,7 +3,21 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "vet_pecialties",
+		joinColumns = @JoinColumn(name = "vet_id"),
+		inverseJoinColumns = @JoinColumn(name = "specialty_id"))
 	private Set<Specialty> specialties = new HashSet<>();
 
 	public Set<Specialty> getSpecialties() {
