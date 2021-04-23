cfa1442f7697efe6ac90a61e4a9cae5fcd1c1b9a
Sebastián Perez Ferretti <sperez@bykom.com>
2021-04-23 17:17:58 -0300

    Servicios no tenian la anotacion de servicio ni el profile adecuado

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.services.PetService;
 
+@Service
+@Profile("springdatajpa")
 public class PetSDJpaService extends AbstractJpaService<Pet, PetRepository> implements PetService{
 
 	public PetSDJpaService(PetRepository repository) {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
 
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService extends AbstractJpaService<PetType, PetTypeRepository> implements PetTypeService{
 
 	public PetTypeSDJpaService(PetTypeRepository repository) {
